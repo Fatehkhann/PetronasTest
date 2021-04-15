7e9db5347c01c1a5549112f8e7e6dc30072cf83f
Fateh Khan <fatehkhann@gmail.com>
2021-04-15 22:10:46 +0500

    Initial Commit

---
+++ PetronasTest/PetronasTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetronasTestApp.swift
+//  PetronasTest
+//
+//  Created by Fateh Khan on 15/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct PetronasTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
